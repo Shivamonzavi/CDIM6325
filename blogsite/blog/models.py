3fd53e7bbce5bd7c465bda0ed43dc0724a859095
Shiva <smashaallah1@buffs.wtamu.edu>
2024-09-08 23:07:24 -0700

    p 48

--- blogsite/blog/models.py
+++ blogsite/blog/models.py
@@ -2,34 +2,47 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+
 # Create your models here.
 class Post(models.Model):
 
+    # Define Status Choices
     class Status(models.TextChoices):
-        DRAFT = 'DF' , 'Draft'
-        PUBLISHED = 'PB' , 'Published'
+        DRAFT = "DF", "Draft"
+        PUBLISHED = "PB", "Published"
+
+    # Model Fields
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date="publish")
     author = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name='blog_posts'
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="blog_posts"
     )
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(
-    max_length=2,
-    choices=Status,
-    default=Status.DRAFT
- )
+        max_length=2,
+        choices=Status.choices,  # Use Status.choices instead of Status
+        default=Status.DRAFT,
+    )
 
+    # Custom Manager
+    class PublishedManager(models.Manager):
+        def get_queryset(self):
+            return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+    # Model Managers
+    objects = models.Manager()  # Default manager
+    published = PublishedManager()  # Custom manager for published posts
+
+    # Meta Options
     class Meta:
-        ordering = ['-publish']
+        ordering = ["-publish"]
         indexes = [
-            models.Index(fields=['-publish'])
+            models.Index(fields=["-publish"]),
         ]
+
+    # String Representation
     def __str__(self):
-        return self.title 
-    
+        return self.title
