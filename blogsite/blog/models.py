82d45646f8bb21b3b5228eb399a4c4d0f172f9e2
Shiva <smashaallah1@buffs.wtamu.edu>
2024-09-17 15:46:00 -0700

    p100

--- blogsite/blog/models.py
+++ blogsite/blog/models.py
@@ -14,10 +14,7 @@ class Post(models.Model):
 
     # Model Fields
     title = models.CharField(max_length=250)
-    slug = models.SlugField(
-        max_length=250, 
-        unique_for_date="publish"
-    )
+    slug = models.SlugField(max_length=250, unique_for_date="publish")
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="blog_posts"
     )
@@ -28,7 +25,7 @@ class Post(models.Model):
     status = models.CharField(
         max_length=2,
         choices=Status.choices,  # Use Status.choices instead of Status
-        default=Status.DRAFT
+        default=Status.DRAFT,
     )
 
     # Custom Manager
@@ -43,9 +40,7 @@ class Post(models.Model):
     # Meta Options
     class Meta:
         ordering = ["-publish"]
-        indexes = [
-            models.Index(fields=["-publish"])
-        ]
+        indexes = [models.Index(fields=["-publish"])]
 
     # String Representation
     def __str__(self):
@@ -54,5 +49,27 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse(
             "blog:post_detail",
-            args=[self.publish.year, self.publish.month, self.publish.day, self.slug]
+            args=[self.publish.year, self.publish.month, self.publish.day, self.slug],
         )
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['created']
+        indexes = [
+            models.Index(fields=['created']),
+        ]
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
--- blogsite/blog/views.py
+++ blogsite/blog/views.py
@@ -1,40 +1,57 @@
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
+from django.views.decorators.http import require_POST
 from .models import Post
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import CommentForm, EmailPostForm
+from django.core.mail import send_mail
+
+
 def post_share(request, post_id):
     # Retrieve post by id
-    post = get_object_or_404(
-        Post,
-        id=post_id,
-        status=Post.Status.PUBLISHED
-    )
-    if request.method == 'POST':
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+
+    if request.method == "POST":
         # Form was submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
             # Form fields passed validation
             cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = (
+                f"{cd['name']} ({cd['email']}) " f"recommends you read {post.title}"
+            )
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']}'s comments: {cd['comments']}"
+            )
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=None,
+                recipient_list=[cd["to"]],
+            )
+            sent = True
             # ... send email
     else:
         form = EmailPostForm()
     return render(
-        request,
-        'blog/post/share.html',
-        {
-            'post': post,
-            'form': form  
-        }
+        request, "blog/post/share.html", {"post": post, "form": form, "sent": sent}
     )
+
+
 class PostListView(ListView):
     """
     Alternative post list view
     """
+
     queryset = Post.published.all()
-    context_object_name = 'posts'
+    context_object_name = "posts"
     paginate_by = 3
-    template_name = 'blog/post/list.html'
+    template_name = "blog/post/list.html"
+
+
 # Create your views here.
 def post_list(request):
     post_list = Post.published.all()
@@ -45,15 +62,11 @@ def post_list(request):
         posts = paginator.page(page_number)
     except PageNotAnInteger:
         # If page_number is not an integer get the first page
-        posts = paginator.page(1)    
+        posts = paginator.page(1)
     except EmptyPage:
         # If page_number is out of range get last page of results
         posts = paginator.page(paginator.num_pages)
-    return render(
-        request,
-        "blog/post/list.html", 
-        {"posts": posts}
-    )
+    return render(request, "blog/post/list.html", {"posts": posts})
 
 
 def post_detail(request, year, month, day, post):
@@ -63,9 +76,45 @@ def post_detail(request, year, month, day, post):
         slug=post,
         publish__year=year,
         publish__month=month,
-        publish__day=day)
+        publish__day=day,
+    )
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+    # Form for users to comment
+    form = CommentForm()
+
     return render(
-        request, 
-        "blog/post/detail.html",
-        {"post": post}
+        request,
+        "blog/post/detail.html", 
+        {
+        "post": post,            
+        'comments': comments,
+        'form': form
+        }
+    )
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+    comment = None
+    # A comment was posted
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # Create a Comment object without saving it to the database
+        comment = form.save(commit=False)
+        # Assign the post to the comment
+        comment.post = post
+        # Save the comment to the database
+        comment.save()
+    return render(
+        request,
+        'blog/post/comment.html',
+        {
+            'post': post,
+            'form': form,
+            'comment': comment
+        }
     )
