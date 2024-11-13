56b78cd9f19b0a0210a61bde31cdea7297ada758
Shiva <smashaallah1@buffs.wtamu.edu>
2024-11-12 22:11:54 -0800

    p 671

---
+++ educa/courses/api/views.py
@@ -0,0 +1,51 @@
+from django.db.models import Count
+from django.shortcuts import get_object_or_404
+from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from courses.models import Course, Subject
+from courses.api.pagination import StandardPagination
+from courses.api.serializers import CourseSerializer, SubjectSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+
+class SubjectListView(generics.ListAPIView):
+    queryset = Subject.objects.annotate(total_courses=Count('courses'))
+    serializer_class = SubjectSerializer
+    pagination_class = StandardPagination
+
+class SubjectDetailView(generics.RetrieveAPIView):
+    queryset = Subject.objects.annotate(total_courses=Count('courses'))
+    serializer_class = SubjectSerializer
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.prefetch_related('modules')
+    serializer_class = CourseSerializer
+    pagination_class = StandardPagination
+
+    @action(
+        detail=True,
+        methods=['post'],
+        authentication_classes=[BasicAuthentication],
+        permission_classes=[IsAuthenticated]
+    )
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
+        course.students.add(request.user)
+        return Response({'enrolled': True})
+    
+class SubjectViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Subject.objects.annotate(total_courses=Count('courses'))
+    serializer_class = SubjectSerializer
+    pagination_class = StandardPagination
+
+#class CourseEnrollView(APIView):
+    #authentication_classes = [BasicAuthentication]
+    #permission_classes = [IsAuthenticated]
+    
+    def post(self, request, pk, format=None):
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'enrolled': True})
